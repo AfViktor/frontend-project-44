9e6f45429eea023c21678355b78a1518707ea120
Vik <vik@MacBook-Air-Vik.local>
2023-07-02 13:29:18 +0300

    update 2 steps

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 
+import greetings from './Users/vik/study/hexlet/projects/mindgames/frontend-project-44/src/cli.js';
+
 console.log('Welcome to the Brain Games!');
+
+greetings();
