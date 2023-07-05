2b5e0d945359617909a5f84c3059f28cb6b6b434
Vik <vik@MacBook-Air-Vik.local>
2023-07-05 17:47:16 +0300

    update

--- src/index.js
+++ src/index.js
@@ -3,38 +3,37 @@
 import readlineSync from 'readline-sync';
 import askName from '../src/cli.js';
 
-
 const brainEven = () => {
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  function getRandom () {
-    return Math.round(Math.random()*100);
-  };
-  console.log(getRandom());
-
-  let randomInt = getRandom();
+  let name = askName();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  for (let i = 1; i <= 3; i++){
+    function getRandom () {
+      return Math.round(Math.random()*100);
+    };
   
-  const getAnswer = () => {
-    let answer = readlineSync.question('Your answer: ');
-    return answer;
-  };
+    let randomInt = getRandom();
+    console.log(randomInt);
+    
+    const getAnswer = () => {
+      let answer = readlineSync.question('Your answer: ');
+      return answer;
+    };
+    
+    let answer = getAnswer();
+    let correctAnswer = '';
   
-  let answer = getAnswer();
-  let correctAnswer = 'yes';
-  let unCorrectAnswer = 'no';
-
-  if (randomInt % 2 == 0) {
-    console.log('correct');
-  }
-
-  if (randomInt % 2 === 0) {
-    if (answer === correctAnswer){
-      console.log('Correct!');
+    if (randomInt % 2 === 0) {
+      correctAnswer = 'yes';
     } else {
-      console.log(`${answer} is wrong answer ;(. Correct answer was ${unCorrectAnswer}.`);
+      correctAnswer = 'no';
     }
-  }  
+    if (answer === correctAnswer) {
+      console.log('Correct!')
+    } else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+    }
+    console.log(`Congratulations, ${name}`);
+  }
 }
-
-
+  
 export default brainEven;
