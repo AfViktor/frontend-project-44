d22ef9d784e5d6d5728d4e0371ef5ac8556b3d87
Vik <vik@MacBook-Air-Vik.local>
2023-07-05 15:05:05 +0300

    update

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -7,23 +7,25 @@ console.log('Welcome to the Brain Games!');
 console.log(`Hello, ${askName()}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-function getRandom () {
-  let randomInt = Math.round(Math.random()*100);
-  return randomInt;
-};
-console.log(getRandom());
-
-const getAnswer = () => {
-  let answer = readlineSync.question('Your answer: ');
-  return answer;
-};
-
-getAnswer();
-
-if (getRandom() % 2 === 0) {
-  if (getAnswer() === 'yes'){
-    console.log('Correct!');
-  } else {
-    console.log(`${answer} is wrong answer ;(. Correct answer was ${notEven}.`);
-  }
+for (let i = 1; i <= 3; i++) {
+  function getRandom () {
+    let randomInt = Math.round(Math.random()*100);
+    return randomInt;
+  };
+  console.log(getRandom());
+  
+  const getAnswer = () => {
+    let answer = readlineSync.question('Your answer: ');
+    return answer;
+  };
+  
+  getAnswer();
+  
+  if (getRandom() % 2 === 0) {
+    if (answer === 'yes'){
+      console.log('Correct!');
+    } else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${notEven}.`);
+    }
+  }  
 }
