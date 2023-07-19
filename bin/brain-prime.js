b93768f34a4c3e409591042ca6b2877588ed2b03
Vik <vik@MacBook-Air-Vik.local>
2023-07-19 16:22:57 +0300

    update games

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -4,4 +4,4 @@ import { gameIsPrime } from '../src/index.js'
 
 console.log('Welcome to the Brain Games!');
 
-gameIsPrime();
+gameIsPrime(3);
--- src/index.js
+++ src/index.js
@@ -10,12 +10,13 @@ import { isPrime } from "../src/games/brainPrime.js"
 
 function runGameBrainEven(description, getRoundGame) {
   let name = askName();
+  console.log(`Hello, ${name}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   for (let i = 0; i < getRoundGame; i++) {
     let dataIsEven = isEven();
     let correctAnswer = dataIsEven[1];
     let randomInt = dataIsEven[0];
-    console.log(randomInt);
+    console.log(`Question: ${randomInt}`);
     let answer = readlineSync.question('Your answer: ');
     if (answer !== correctAnswer) {
       return console.log(`${answer} is wrong. Correct answer was ${correctAnswer}. Try again ${name}`);
@@ -23,20 +24,21 @@ function runGameBrainEven(description, getRoundGame) {
       console.log('Correct!');
     };
     if (i === 2) {
-      console.log(`Congratulations, ${name}`);
+      console.log(`Congratulations, ${name}!`);
     }
   };
 };
 
 function runGameCalc (getRoundGame) {
   let name = askName();
+  console.log(`Hello, ${name}!`);
   console.log('What is the result of the expression?');
   for (let i = 0; i < getRoundGame; i++) {
     let counter = randomInt(1,3);
     switch(counter) {
     case 1: 
       let dataIsEvenSum = isEvenSum();
-      console.log(`${dataIsEvenSum[0]} + ${dataIsEvenSum[1]}: `);
+      console.log(`Question: ${dataIsEvenSum[0]} + ${dataIsEvenSum[1]}: `);
       let answerSum = Number(readlineSync.question('Your answer: '));
       if (answerSum !== dataIsEvenSum[2]) {
         return console.log(`${answerSum} is wrong. Correct answer was ${dataIsEvenSum[2]}. Try again ${name}`);
@@ -46,7 +48,7 @@ function runGameCalc (getRoundGame) {
       break;
     case 2:
       let dataIsEvenSubtract = isEvenSubtract();
-      console.log(`${dataIsEvenSubtract[0]} - ${dataIsEvenSubtract[1]}: `);
+      console.log(`Question: ${dataIsEvenSubtract[0]} - ${dataIsEvenSubtract[1]}: `);
       let answerSubtract = Number(readlineSync.question('Your answer: '));
       if (answerSubtract !== dataIsEvenSubtract[2]) {
         return console.log(`${answerSubtract} is wrong. Correct answer was ${dataIsEvenSubtract[2]}. Try again ${name}`);
@@ -56,7 +58,7 @@ function runGameCalc (getRoundGame) {
       break;
     case 3:
       let dataIsEvenMulti = isEvenMulti();
-      console.log(`${dataIsEvenMulti[0]} * ${dataIsEvenMulti[1]}: `);
+      console.log(`Question: ${dataIsEvenMulti[0]} * ${dataIsEvenMulti[1]}: `);
       let answerMulti = Number(readlineSync.question('Your answer: '));
       if (answerMulti !== dataIsEvenMulti[2]) {
         return console.log(`${answerMulti} is wrong. Correct answer was ${dataIsEvenMulti[2]}. Try again ${name}`);
@@ -66,7 +68,7 @@ function runGameCalc (getRoundGame) {
       break;
   }; 
   if (i === 2) {
-    console.log(`Congratulations, ${name}`);
+    console.log(`Congratulations, ${name}!`);
   }    
   };
 };
@@ -75,13 +77,14 @@ export { runGameBrainEven, runGameCalc};
 
 function runGameNode (getRoundGame) {
   let name = askName();
+  console.log(`Hello, ${name}!`);
   console.log('Find the greatest common divisor of given numbers.');
   for (let i = 0; i < getRoundGame; i++) {
     let dataIsEvenNod = isEvenNod();
     let a = dataIsEvenNod[0];
     let b = dataIsEvenNod[1];
     let divider = getDivider(a, b);
-    console.log(`${a} ${b}`);
+    console.log(`Question: ${a} ${b}`);
     let answer = Number(readlineSync.question('Your answer: '));
     if (answer !== divider) {
       return console.log(`${answer} is wrong. Correct answer was ${divider}. Try again ${name}`);
@@ -89,7 +92,7 @@ function runGameNode (getRoundGame) {
           console.log('Correct!');
     }
     if (i === 2) {
-      console.log(`Congratulations, ${name}`);
+      console.log(`Congratulations, ${name}!`);
     }
   }
 }
@@ -98,12 +101,13 @@ export { runGameNode }
 
 function runGameProgressiv (getRoundGame) {
   let name = askName();
+  console.log(`Hello, ${name}!`);
   console.log('What number is missing in this progression?   ');
   for (let i = 0; i < getRoundGame; i++) {
     let dataProgressive = replaceElement();
     let showProgressive = dataProgressive[0].join(' ');
     let rightAnswer = dataProgressive[1];
-    console.log(showProgressive);
+    console.log(`Question: ${showProgressive}`);
     let answer = Number(readlineSync.question('Your answer: '));
     if (answer !== rightAnswer) {
       return console.log(`${answer} is wrong. Correct answer was ${rightAnswer}. Try again ${name}`);
@@ -111,27 +115,32 @@ function runGameProgressiv (getRoundGame) {
           console.log('Correct!');
     }
     if (i === 2) {
-      console.log(`Congratulations, ${name}`);
+      console.log(`Congratulations, ${name}!`);
     }
   }
 }
 
 export { runGameProgressiv }
 
-function gameIsPrime () {
+function gameIsPrime (getRoundGame) {
   let name = askName();
+  console.log(`Hello, ${name}!`);
   console.log('Answer yes if given number is prime. Otherwise answer no.');
-  let dataIsPrime = isPrime();
-  let showNumber = dataIsPrime[0];
-  let rightAnswer = dataIsPrime[1];
-  console.log(showNumber);
-  let answer = readlineSync.question('Your answer: ');
-    if (answer !== rightAnswer) {
-      return console.log(`${answer} is wrong. Correct answer was ${rightAnswer}. Try again ${name}`);
-        } else {
-          console.log('Correct!');
-          console.log(`Congratulations, ${name}`);
+  for (let i = 0; i < getRoundGame; i++) {
+    let dataIsPrime = isPrime();
+    let showNumber = dataIsPrime[0];
+    let rightAnswer = dataIsPrime[1];
+    console.log(`Question: ${showNumber}`);
+    let answer = readlineSync.question('Your answer: ');
+      if (answer !== rightAnswer) {
+        return console.log(`${answer} is wrong. Correct answer was ${rightAnswer}. Try again ${name}`);
+      } else {
+        console.log('Correct!');
     }
+    if (i === 2) {
+      console.log(`Congratulations, ${name}!`);
+    }
+  }
 }
 
 export { gameIsPrime }
