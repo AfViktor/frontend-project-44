4a1a3cfcef957de17fff68b76e0bce37f448e061
Vik <vik@MacBook-Air-Vik.local>
2023-07-21 20:04:45 +0300

    рефактор всего кода игр

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 import runGame from '../src/index.js';
-import isEvenNod from '/Users/vik/study/hexlet/projects/mindgames/frontend-project-44/src/games/brainNod.js';
+import isEvenNod from '../src/games/brainNod.js';
 
 runGame('Find the greatest common divisor of given numbers.', isEvenNod);
