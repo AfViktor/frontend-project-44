5ab7dafbe5cf00b634a4a5a5cd44cb9469d1e42f
Vik <vik@MacBook-Air-Vik.local>
2023-07-21 20:08:33 +0300

    рефактор всего кода игр

--- bin/brain-progres.js
+++ bin/brain-progres.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 import runGame from '../src/index.js';
-import makeProgress from  '/Users/vik/study/hexlet/projects/mindgames/frontend-project-44/src/games/brainProgres.js';
+import makeProgress from  '../src/games/brainNod.js';
 
 runGame('What number is missing in the progression?   ', makeProgress);
