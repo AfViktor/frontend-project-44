339fe2c2d4e8310f731e1b326a18390c6896a7dd
Vik <vik@MacBook-Air-Vik.local>
2023-07-21 19:58:30 +0300

    рефактор всего кода игр

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { runGame } from '../src/index.js';
+import runGame from '../src/index.js';
 import isCalc from '../src/games/brainCalc.js';
 
 runGame('What is the result of the expression?', isCalc);
--- bin/brain-progres.js
+++ bin/brain-progres.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { runGame } from '../src/index.js';
+import runGame from '../src/index.js';
 import makeProgress from  '/Users/vik/study/hexlet/projects/mindgames/frontend-project-44/src/games/brainProgres.js';
 
 runGame('What number is missing in the progression?   ', makeProgress);
