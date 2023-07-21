03487fcb79765b851f8b8408cb8b954b43764049
Vik <vik@MacBook-Air-Vik.local>
2023-07-21 20:31:42 +0300

    update

--- src/games/brainPrime.js
+++ src/games/brainPrime.js
@@ -4,7 +4,7 @@ function isPrime() {
   const number = randomInt(1, 100);
   let question = '';
   for (let i = 2; i < number; i += 1) {
-    if (number % i === 0) {
+    if (number % i === 0 && number % number === 0) {
       question = `Question: ${number}`;
       return [question, 'no'];
     }
