1b4c3a09706d428259e859e2c97553fa092c4618
Viktor <afonkovviktor@yandex.ru>
2023-07-25 16:56:32 +0300

    update

--- src/games/brainPrime.js
+++ src/games/brainPrime.js
@@ -7,17 +7,17 @@ function getQuestion(number) {
 function isPrime(number) {
   for (let i = 2; i < number; i += 1) {
     if (number % i === 0) {
-      return 'yes';
+      return false;
     }
   }
-  return 'no';
+  return number > 1;
 }
 
 function runGamePrime() {
   const number = getRandomInt(1, 100);
   let question = '';
   let rightAnswer = '';
-  rightAnswer = isPrime(number);
+  rightAnswer = isPrime(number) ? 'yes' : 'no';
   question = getQuestion(number);
   return [question, rightAnswer];
 }
